ddf5e5d6a1325490572bda76916680b7f3e8befb
luiz.renato <luiz.renato@NB-CUSTA0331>
2019-07-24 15:07:53 -0300

    Teste

--- site_exemplo/js/lrscript.js
+++ site_exemplo/js/lrscript.js
@@ -1,7 +1,7 @@
 ﻿$(function () {
     $("#btnBuscarLoc").click(function () {
         var originbtn = this.innerHTML;
-        this.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...';
+        this.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Carregando...';
 
         $("#generated").hide();
         $('#generated tbody').empty();
